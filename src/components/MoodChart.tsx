588e6fbba415e60b3ac84a0a838fe22316fbb740
Pietro Russo <pietro.russo.d@gmail.com>
2021-03-03 16:58:52 +0100

    Add a chart with plotly.js

--- src/components/MoodChart.tsx
+++ src/components/MoodChart.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
-interface MoodChartProps {  
+interface MoodChartProps {
     [key: string]: number
 }
 
-function MoodChart(props:MoodChartProps){
-    return (<pre>{JSON.stringify(props)}</pre>)
-}
+function MoodChart(props: MoodChartProps) {
+
+    return (
+        <>
+            <Plot
+                data={[
+                    {type: 'bar', x: Object.keys(props), y: Object.values(props)},
+                ]}
+                layout={ {width: 800, height: 400, title: 'A Mood Chart'} }
+            />
+            <pre>{JSON.stringify(props)}</pre>
+        </>)
+};
 
 export default MoodChart
