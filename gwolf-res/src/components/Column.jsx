3916d471bab3b3a4feab3ca417f937aeae69e528
Antonio Bugni <antonio.bugni@d4next.com>
2021-05-31 08:43:36 +0200

    Add accordion mode to show and hide users Add register button

--- gwolf-res/src/components/Column.jsx
+++ gwolf-res/src/components/Column.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {Button} from "react-bootstrap";
 
 export default function Column(props) {
+
+    const [visibleUsers, setVisibleUsers] = useState(false);
+
+    const showUsers = () => {
+        setVisibleUsers(!visibleUsers)
+    };
+
+    const register = (day) => {
+       alert(day)
+    };
+
     return (
         <>
-            <div className={"row"}>
-                <div className={"col-3"}>{props.content.day}</div>
-                <div className={"col-3"}>{props.content.type}</div>
+            <div className={"row gw-row"} onClick={() => showUsers()}>
+                <div className={"col-2"}>{props.content.day}</div>
+                <div className={"col-8"}>{props.content.type}</div>
             </div>
-            <div className={"row"}>
+            {visibleUsers && <div className={"row"}>
                 {props.content && props.content.users &&
                 <div>
                     {props.content.users.map((user) => (
                         <div className={"col-6"} key={user}>{user}</div>
                         ))}
                 </div>}
-            </div>
+            </div>}
+            <Button onClick={() => register(props.content.day)}>Registrati</Button>
         </>
     );
 };
