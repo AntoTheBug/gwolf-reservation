e3538a18c12e0e364814266ff0dcfbf33f9ee9d5
Anto <antoniobugni@unobravo.com>
2023-01-03 14:20:08 +0100

    Add dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Home from "./components/Home";
 import Workout from "./components/Workout";
 import Week from "./components/Week";
 import NavBar from "./components/NavBar";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import WorkoutDay from "./components/WorkoutDay";
+import darkMode from "./images/dark.png";
 
 function App() {
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    };
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
   return (
-      <div className="App">
+      <div className={`App ${theme}`}>
           <Router>
               <NavBar />
               <div className="pages">
@@ -21,6 +34,10 @@ function App() {
                   </Routes>
               </div>
           </Router>
+
+          <div className="darkMode" onClick={toggleTheme}>
+              <img src={darkMode} alt="darkmode" width="40px" height="40px"/>
+          </div>
       </div>
   );
 };
